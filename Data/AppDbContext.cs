4cd767580f1db77ed94759d0dfdd70e3507571d0
KubaCeran <kuba.ceran@gmail.com>
2024-04-16 18:44:43 +0200

    Db added

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using GrpcDemo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GrpcDemo.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext() : base()
+        {
+        }
+
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        DbSet<ToDoItem> ToDoItems { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,13 +1,15 @@
+using GrpcDemo.Data;
 using GrpcDemo.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite("DataSource=ToDoDatabase.db"));
+
 builder.Services.AddGrpc();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
